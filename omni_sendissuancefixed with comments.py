adbfedb516d48107e139a11e76a7cd1cebab748c
martinseeger2002 <57598081+martinseeger2002@users.noreply.github.com>
2022-12-17 19:53:43 -0600

    Update omni_sendissuancefixed with comments.py

--- omni_sendissuancefixed with comments.py
+++ omni_sendissuancefixed with comments.py
@@ -1,10 +1,11 @@
+
 import time
 import ast
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 # rpc_user and rpc_password are set in the litecoin.conf file
-rpc_user = 'YOUR_RPC_USERNAME'
-rpc_password = 'YOUR_RPC_PASSWORD'
+rpc_user = 'user'
+rpc_password = 'password'
 
 # Set up an AuthServiceProxy object to interact with the local Litecoin daemon
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:9332"%(rpc_user, rpc_password))
@@ -17,18 +18,15 @@ arguments_data = f.read()
 # Parse arguments_data as a dictionary
 sif_dict = ast.literal_eval(arguments_data)
 f.close()
-
 # Prompt the user for the serial number of the first token to be issued
 user_input = input("Enter 0 for new token series or enter the number of the last token to be succsefully created to continue series.\n:")
 serial_num = int(user_input)+100001
-
 # Calculate the number of tokens to be issued and the maximum serial number
 num_of_data = (len(sif_dict.keys())-2)
 sn = num_of_data//4
 snf = num_of_data/4
 max_serial_num = 100000+sn-1
 sns = (str(snf)[-2:])
-
 # Read in the address, name, and other information for the tokens
 address = (sif_dict['address'])
 name = (sif_dict['name'])
@@ -36,6 +34,7 @@ esys = int(sif_dict['esys'])
 ttype = int(sif_dict['ttype'])
 preid = int(sif_dict['preid'])
 amount = str(sif_dict['amount'])
+
 
 # Loop through each token to be issued
 while serial_num <= max_serial_num:
@@ -49,11 +48,9 @@ while serial_num <= max_serial_num:
         d3 = (sif_dict[data_num])
         data_num = str(serial_num)[-5:]+'d4'
         d4 = (sif_dict[data_num])
-        
         # Call the omni_sendissuancefixed method to create and issue the token
-        result = rpc_connection.omni_sendissuancefixed(address,esys,ttype,preid,d1,d2,name+str(serial_num)[-5:],d3,d4,amount)
+        result = rpc_connection.omni_sendissuancefixed(address,esys,ttype,preid,d1,d2,name+str(serial_num)[-5:],d3,d4,amount) #rpc_connection.omni_sendissuancefixed
         print(result)
-        
         # Increment the serial number if the call was successful
         if len(result) == 64:
             serial_num += 1
@@ -67,14 +64,10 @@ while serial_num <= max_serial_num:
         d3 = (sif_dict[data_num])
         data_num = str(serial_num)[-5:]+'d4'
         d4 = (sif_dict[data_num])
-        
         # Call the omni_sendissuancefixed method to create and issue the token
-        result = rpc_connection.omni_sendissuancefixed(address,esys,ttype,preid,d1,d2,name+str(serial_num)[-5:],d3,d4,amount)
+        result = rpc_connection.omni_sendissuancefixed(address,esys,ttype,preid,d1,d2,name+str(serial_num)[-5:],d3,d4,amount) #rpc_connection.omni_sendissuancefixed
         print(result)
-        
-        # Break out of the loop if the call was successful
-        if len(result) == 64:
-            break
+        break
 
 if sns == '75':
     serial_num = max_serial_num+1
@@ -84,7 +77,7 @@ if sns == '75':
     d2 = (sif_dict[data_num])
     data_num = str(serial_num)[-5:]+'d3'
     d3 = (sif_dict[data_num])
-    result = rpc_connection.omni_sendissuancefixed(address,esys,ttype,preid,d1,d2,name+str(serial_num)[-5:],d3,d4,amount) #rpc_connection.omni_sendissuancefixed
+    result = rpc_connection.omni_sendissuancefixed(address,esys,ttype,preid,d1,d2,name+str(serial_num)[-5:],d3,'',amount) #rpc_connection.omni_sendissuancefixed
     print(result)
 if sns == '.5':
     serial_num = max_serial_num+1
